5dc97b80e1fea8c72a50af0d121a110985210317
danzk <danzk@WNB30306.bboffice.baur.net>
2019-04-09 08:27:06 +0200

    Ver- & Entschlüsselung

--- src/server/utils/EncryptUtils.java
+++ src/server/utils/EncryptUtils.java
@@ -1,5 +1,22 @@
-package server.utils;
+package src.server.utils;
+
 
 public class EncryptUtils {
+	public static void main(String[] args) {
+		EncryptUtils test = new EncryptUtils();
+		test.getEncryptedString("ich bin ein Satz");
+	}
+	
+	/*
+	 * Encrypts the given String
+	 */
+	public String getEncryptedString(String username) {
+		StringBuilder sb = new StringBuilder();
+		for(Character c: username.toCharArray()) {
+			sb.append((char)((c.hashCode()+username.length())*username.length()));
+		}
+		System.out.println(sb.toString());
+		return sb.toString();
+	}
 
 }
