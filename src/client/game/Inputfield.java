9c1bd2cb761c39ff69ab762fc06c1803faf2e8ca
danzk <danzk@WNB30306.bboffice.baur.net>
2019-04-10 12:01:30 +0200

    Solving of problems in drawing
    Render berechnet nun die mittlere Bidlschirmposition um das currentWord
    mittig zu positionieren
    
    beim zeichnen wird auf breitere Buchstaben (w) geachtet und die breite
    des bereiches zwischen den buchstaben angepasst.
    deleteLastChar durchsucht jetzt das CurrentWord nach dem letzten
    vorkommenden Buchstaben und "löscht" diesen

--- src/client/game/Inputfield.java
+++ src/client/game/Inputfield.java
@@ -9,7 +9,6 @@ import client.game.screens.Gamescreen;
 public class Inputfield {
 	//Word to resolve
 	private String word;
-	//available chars 
 	//Array which contains input of user(char)
 	private char[] currentWord;
 	//indicates the place in resolveAr
@@ -30,27 +29,38 @@ public class Inputfield {
 
 	public void render(Graphics g) {
 		g.getFontMetrics();
-		
-		int x = 50;
-		int y = 600;
+		int charWidth = gamescreen.getContainer().getFontUtils().getMatrixFont().getSize();
+		int x = ((gamescreen.getContainer().getWidth())-(gamescreen.getContainer().getWidth()%10))/2-((charWidth*(word.length()-word.length()%2))/2)-(charWidth*2);
+		int y = gamescreen.getContainer().getHeight()-(gamescreen.getContainer().getHeight()/15);
 		g.setColor(Color.white);
-		for(char c: currentWord) {
-			g.drawString(String.valueOf(c), x+=30, y);
+		for(int i = 0; i < currentWord.length;i++) {
+			if(currentWord[i]=='w') {
+				g.drawString(String.valueOf(currentWord[i]), x+=charWidth, y);
+				charWidth = gamescreen.getContainer().getFontUtils().getMatrixFont().getSize()+5;
+			}else {
+				g.drawString(String.valueOf(currentWord[i]), x+=charWidth, y);
+				charWidth = gamescreen.getContainer().getFontUtils().getMatrixFont().getSize();
+			}
 		}
 	}
 	
 	//deletes the last char and reduces counter
 	public void deleteLastChar() {
-		currentWord[currentPosition] = '_'; 
-		if(currentPosition > 0 && currentPosition <= word.length()+1) {
-			currentPosition--;
+		for(int i = currentWord.length-1; i >=0 ; i--) {
+			if(currentWord[i] !='_') {
+				currentWord[i] ='_';
+				currentPosition = i;
+				break;
+			}
 		}
+	
 	}
 	//adds char to array and raises counter
 	public void addCharToArr(char character) {
-		currentWord[currentPosition] = character;
-		if(currentPosition >=0 && currentPosition < word.length()-1) {
-			currentPosition++;
+		if(currentPosition >= 0 && currentPosition <=word.length()-2) {
+			currentWord[currentPosition++] = character;
+		}else {
+			currentWord[currentPosition] = character;
 		}
 	}
 	
